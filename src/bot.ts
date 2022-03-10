8947d75be8c31e8680b3fb9f5e36928b00050a2a
Auriane <auriane@uccelolabs.com>
2022-03-10 19:40:42 +0100

    feat: update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -8,22 +8,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.listen(process.env.PORT || 3000);
 
-app.use(express.static(path.join(process.cwd, 'src'))); // ajouter un middleware 
+//app.use(express.static(path.join(process.cwd, 'src'))); // ajouter un middleware 
 // à la place de __dirname on peut mettre process.cwd --> chemin depuis lequel on a exécuté le fichier point d'entrée (chemin terminal))
 
-// app.get('/', (req, res, next) => {
-//     //oui; // démo pour montrer que la middleware fonctionne
-//     return res.sendFile(path.join(__dirname, './index.html')); // renvoie une page html basique avec écrit "lol"
+//For avoidong Heroku $PORT error
+// app.get('/', function(request, response) {
+//     var result = 'App is running'
+//     response.send(result);
+// }).listen(app.get('port'), function() {
+//     console.log('App is running, server is listening on port ', app.get('port'));
 // });
-// app.get() => {
-//     return res.sendFile(path.join(__dirname, './index.html')); 
-// }
 
-app.listen(port, () => {
-    console.log('App running on port: ' + port);
-  });
+// app.listen(port, () => {
+//     console.log('App running on port: ' + port);
+//   });
 
+// http.createServer(Request).listen(process.env.PORT || 6000);
 const botToken = process.env.botToken;
 
 const client = new Client({
