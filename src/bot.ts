de309122dd84c95868e302eb06e69702bd0dc741
Auriane <auriane@uccelolabs.com>
2022-03-10 19:11:29 +0100

    feat: config port

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,14 @@ import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
 require("dotenv").config();
 
+const path = require('path')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('App running on port: ' + port);
+  });
 
 const botToken = process.env.botToken;
 
