4fcabd4439749e87ada00529ed2c84de82cf8c89
Auriane <auriane@uccelolabs.com>
2022-03-10 19:47:57 +0100

    feat: update bots.ts

--- src/bot.ts
+++ src/bot.ts
@@ -8,8 +8,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 3000);
+app.get('/index.html').listen(app.get('port'), function() {
+        console.log('App is running, server is listening on port ', app.get('port'));
+    });
 
+app.listen(port, () => {
+    console.log('App running on port: ' + port);
+  });
 //app.use(express.static(path.join(process.cwd, 'src'))); // ajouter un middleware 
 // à la place de __dirname on peut mettre process.cwd --> chemin depuis lequel on a exécuté le fichier point d'entrée (chemin terminal))
 
